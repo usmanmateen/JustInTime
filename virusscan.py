418de2c1be78b172ee3715091ecbae61952ab60d
Vineger2k <vinay2k12s@live.com>
2023-11-20 00:05:17 +0000

    updating virus file

--- virusscan.py
+++ virusscan.py
@@ -1,12 +1,24 @@
-
-
+from flask import Flask, request, jsonify, render_template, redirect, url_for, session
+from werkzeug.utils import secure_filename
+import subprocess
+import os
 
 def namecheck(filename):
+
     extensions = {'txt', 'pdf', 'png', 'jpg', 'jpeg'}
     extension = filename.rsplit('.',1)[1].lower()
     return extension in extensions
 
-
-    #return '.' in filename and filename.rsplit('.', 1)[1].lower() in extensions
-
 print(namecheck("test.gif"))
+
+
+def scan_file(filepath):
+    try:
+        result = subprocess.check_output(['clamscan', '--no-summary', filepath])
+        result = result.decode('utf-8').strip()
+        if "Infected files: 0" in result:
+            return True, "File is clean."
+        else:
+            return False, "Virus detected: " + result
+    except subprocess.CalledProcessError as e:
+        return False, "Error during virus scan: " + str(e)
