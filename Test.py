c9aa437ddd4cd77cb99685ea79c56b9d1c3f52fc
usman <usmanmateen255@gmail.com>
2023-12-09 03:13:47 +0000

    Job Allocations

--- Test.py
+++ Test.py
@@ -405,7 +405,7 @@ def main():
     print("Code Stopped")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
     print("Bye")
 
     
